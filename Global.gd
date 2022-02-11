01c421f8477288df4117ca3144bfacb22404208c
WilliamSandage <97703131+WilliamSandage@users.noreply.github.com>
2022-02-10 23:23:00 -0500

    i dont know anymore

--- Global.gd
+++ Global.gd
@@ -1,8 +1,37 @@
 extends Node
 
-func _ready():
-	randomize()
+var VP = Vector2.ZERO
+var score = 0
+var time = 100
+var lives = 5
 
-func _unhandled_input(event):
-	if event.is_action_pressed("menu"):
-		get_tree().quit()
+
+func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
+	randomize()
+	VP = get_viewport().size
+	var _signal = get_tree().get_root().connect("size_changed", self, "_resize")
+
+func _resize():
+	VP = get_viewport().size
+	
+func reset():
+	score = 0
+	time = 100
+	lives = 5
+
+func update_score(s):
+	score += s
+	var hud = get_node_or_null("root/Game/UI/HUD")
+	if hud != null:
+		hud.update_score()
+func update_time(t):
+	time += t
+	var hud = get_node_or_null("root/Game/UI/HUD")
+	if hud != null:
+		hud.update_time()
+func update_lives(l):
+	lives += l
+	var hud = get_node_or_null("root/Game/UI/HUD")
+	if hud != null:
+		hud.update_lives()
